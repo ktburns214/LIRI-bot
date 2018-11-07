7cace33276958ea741c9d810a47de5a4420cd741
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2018-11-06 21:56:51 -0600

    EOD changes

--- keys.js
+++ keys.js
@@ -1,3 +1,8 @@
+// OMDB key: 65fea01f  
+
+// bands in town API 
+//   https://virtserver.swaggerhub.com/ktburns214/node_homework/1.0.0 
+
 console.log('this is loaded');
 
 exports.spotify = {
--- liri-bot.js
+++ liri-bot.js
@@ -1,3 +1,13 @@
+require("dotenv").config();
+
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var moment = require("moment");
+var keys = require("./keys.js");
+var fs = require("fs");
+
+var spotifyKeys = new Spotify(keys.spotify);
+
 // assumes user set up completed (api keys, etc.)
 
 // get the user input
@@ -5,23 +15,35 @@ const input = process.argv[2];
 
 // make decision based on command
 switch (input) {
-    case "consert-this":
-        consertThis ();
-        break;
-    
+    case "concert-this":
+        concertThis ();
+        break;    
     case "spotify-this-song":
         spotifyThisSong ();
         break;
-
+    case "movie-this":
+        movieThis ();
+        break;
+    case "do-what-it-says":
+        doWhatItSays ();
+        break;
     default: 
         console.log("I don't understand, ask the Foogle-Bot");
         break;
 }
 
-function consertThis () {
-    console.log("Consert this");
+function concertThis () {
+    console.log("Concert this");
 }
 
 function spotifyThisSong () {
     console.log("Spotify this song: " + process.argv[3]);
+}
+
+function movieThis () {
+    console.log("Movie this");
+}
+
+function doWhatItSays () {
+    console.log("Do what it says");
 }
