aa987cb20a838dcc7d61e3f418be6769a825e76e
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-02-25 11:10:09 -0600

    fixed some errors

--- liri-bot.js
+++ liri-bot.js
@@ -1,43 +1,46 @@
 require("dotenv").config();
 
-var Spotify = require("node-spotify-api");
-var request = require("request");
-var moment = require("moment");
-var keys = require("./keys.js");
-var fs = require("fs");
+const Spotify = require("node-spotify-api");
+const request = require("request");
+const moment = require("moment");
+const keys = require("./keys.js");
+const fs = require("fs");
 
-var spotifyKeys = new Spotify(keys.spotify);
+let spotify = new Spotify(keys.spotify);
 
-const input = process.argv[2];
+let inputCommand = process.argv[2];
+let searchParam = process.argv.slice(3).join(" ");
 
-switch (input) {
-    case "concert-this":
-        concertThis ();
-        break;    
-    case "spotify-this-song":
-        spotifyThisSong ();
-        break;
-    case "movie-this":
-        movieThis ();
-        break;
-    case "do-what-it-says":
-        doWhatItSays ();
-        break;
-    default: 
-        console.log("I don't understand, ask the Foogle-Bot");
-        break;
+searchLiri(inputCommand, searchParam.trim());
+
+function searchLiri(command, search) {
+    if (command === "concert-this") {
+        liriConcert(search);
+    } else if (command === "spotify-this-song") {
+        liriSpotify(search);
+    } else if (command === "movie-this") {
+        liriMovie(search);
+    } else if (command === "do-what-it-says") {
+        liriRandom();
+    } else {
+        console.log(`\n\nWrong input.  You're options are: \n- concert-this\n- spotify-this-song\n- movie-this\n- do-what-it-says`);
+    }
 }
 
-function concertThis () {
-    console.log("Concert this");
-    let artist = "";
+function liriConcert(search) {
+    let whichArtist = "";
+    if (search === "") {
+        whichArtist = "Taylor Swift";
+    } else {
+        whichArtist = search;
+    }
 
-    request("https://rest.bandsintown.com/artists/" + artist + "/events/?app_id=codingbootcamp", function (error, response, body) {
+    request("https://rest.bandsintown.com/artists/" + whichArtist + "/events/?app_id=codingbootcamp", function (error, response, body) {
         if (error) {
             console.log(error);
         } else if (response.statusCode === 200) {
             console.log(`\n`);
-            console.log(`SEARCHING FOR ${artist}`)
+            console.log(`SEARCHING FOR ${whichArtist}`)
             console.log(`\n`);
             const results = JSON.parse(body);
 
@@ -73,41 +76,32 @@ function concertThis () {
                     venue: results[i].venue.name,
                     time: timeChange
                 }
-};
 
-function spotifyThisSong () {
-    console.log("Spotify this song: " + process.argv[3]);
-    let song = "";
+                fs.appendFile("log.txt", JSON.stringify(logItem, null, 2), function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
 
-    spotifyKeys.search({ type: "track", query: song }, function (error, data) {
-        if (error) {
-            console.log(error);
+                    else {
+                        
+                    }
+
+                });
+            }
         }
+    });
 
-        console.log(`\n\n`);
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Song: " + data.tracks.items[0].name);
-        console.log("Album: " + data.tracks.items[0].album.name)
-        console.log("Preview Link: " + data.tracks.items[0].preview_url);
-
-        let logItem = {
-            request: "spotify-this-song",
-            artist: data.tracks.items[0].artists[0].name,
-            song: data.tracks.items[0].name,
-            album: data.tracks.items[0].album.name,
-            preview: data.tracks.items[0].preview_url
 }
 
-function movieThis () {
-    console.log("Movie this");
-    let movie = "";
+function liriMovie(search) {
+    let whichMovie = "";
     if (search === "") {
-        movie = "Mr+Nobody";
+        whichMovie = "Mr+Nobody";
     } else {
-        movie = search;
+        whichMovie = search;
     }
 
-    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=65fea01f", function (error, response, body) {
+    request("http://www.omdbapi.com/?t=" + whichMovie + "&y=&plot=short&apikey=168f295", function (error, response, body) {
         if (error) {
             console.log(error);
         } else if (response.statusCode === 200) {
@@ -142,23 +136,74 @@ function movieThis () {
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
 
+            fs.appendFile("log.txt", JSON.stringify(logItem, null, 2), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Content Added to Log!");
+                }
+
+            });
+        }
+    });
 }
 
-function doWhatItSays () {
-    console.log("Do what it says");
-    fs.readFile("random.txt", "utf8", function (error, data) {
+function liriSpotify(search) {
+    let whichSong = "";
+    if (search === "") {
+        whichSong = "The Sign";
+    } else {
+        whichSong = search;
+    }
+
+    spotify.search({ type: "track", query: whichSong }, function (error, data) {
         if (error) {
-            return console.log(error)
-    }
-    
-    data = data.split("\n");
-        const doWhatItSays = Math.floor(Math.floor(Math.random() * 3))
-        let task = data[doWhatItSays].split(",");
-        if (task[0] === "concert-this") {
-            concertThis(task[1]);
-        } else if (task[0] === "spotify-this-song") {
-            spotifyThisSong(task[1]);
-        } else if (task[0] === "movie-this") {
-            movieThis(task[1]);
+            console.log(error);
         }
-    }
+
+        console.log(`\n\n`);
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name)
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+
+        let logItem = {
+            request: "spotify-this-song",
+            artist: data.tracks.items[0].artists[0].name,
+            song: data.tracks.items[0].name,
+            album: data.tracks.items[0].album.name,
+            preview: data.tracks.items[0].preview_url
+        }
+        fs.appendFile("log.txt", JSON.stringify(logItem, null, 2), function(err) {
+            if (err) {
+              console.log(err);
+            }
+          
+            else {
+              console.log("Content Added to Log!");
+            }
+          
+          });
+
+    })
+}
+
+function liriRandom() {
+    fs.readFile("random.text", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        data = data.split("\n");
+        const randomTask = Math.floor(Math.floor(Math.random() * 3))
+        let whichTask = data[randomTask].split(",");
+        if (whichTask[0] === "concert-this") {
+            liriConcert(whichTask[1]);
+        } else if (whichTask[0] === "spotify-this-song") {
+            liriSpotify(whichTask[1]);
+        } else if (whichTask[0] === "movie-this") {
+            liriMovie(whichTask[1]);
+        }
+    });
+}
